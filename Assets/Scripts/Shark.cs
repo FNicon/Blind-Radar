7403201614d16ef2a6b8fe4ce46bced1c610021b
uqsbtysdfjgh <uqsbtysdfjgh@gmail.com>
2018-01-27 03:52:26 +0700

    Improve shark

--- Assets/Scripts/Shark.cs
+++ Assets/Scripts/Shark.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Shark : Fish {
 
+	[Range(0.0f, 100.0f)] public float chanceToRam = 50.0f;
+
 	void RamObject(Vector3 other) {
 		dest = other;
 		StartMoving (speed.runSpeed, GetRunDestination (other));
@@ -12,7 +14,7 @@ public class Shark : Fish {
 	protected new void OnTriggerEnter2D (Collider2D other) {
 		if (other.GetComponent<SubmarineControl> ()) {
 			StopMoving ();
-			if (Random.Range (0.0f, 100.0f) > 50.0f)
+			if (Random.Range (0.0f, 100.0f) <= chanceToRam)
 				RamObject (other.transform.position);
 			else
 				RunFromObject (other.transform.position);
