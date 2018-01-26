d06dc32a91255f4cac26545da53d4ce9e514e473
Reynold Erwandi <reynolderwandi@gmail.com>
2018-01-26 22:58:57 +0700

    Submarine Control

---
+++ Assets/Reynold/Scripts/SubmarineControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SubmarineControl : MonoBehaviour {
+    public Slider horizontalAccelerationSlider;
+    public Slider verticalAccelerationSlider;
+    public float dragDelay = 5f;
+    private Rigidbody2D rb;
+    private float horizontalAcceleration;
+    private float verticalAcceleration;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+        Vector2 desiredVelocity = new Vector2(horizontalAcceleration / 100f, verticalAcceleration / 100f);
+        rb.velocity = desiredVelocity;
+    }
+
+    public void ChangeHorizontalAcceleration()
+    {
+        DOTween.To(() => horizontalAcceleration, x => horizontalAcceleration = x, horizontalAccelerationSlider.value, dragDelay);
+    }
+
+    public void ChangeVerticalAcceleration()
+    {
+        DOTween.To(() => verticalAcceleration, x => verticalAcceleration = x, verticalAccelerationSlider.value, dragDelay);
+    }
+}
