2258a671a5fac53da45c4d6bbf354c2f6a89c167
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-01-27 00:07:58 +0700

    Add Radar

---
+++ Assets/Finiko/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	private SpriteRenderer enemySprite;
+	public float showTime;
+
+	// Use this for initialization
+	void Start () {
+		enemySprite = gameObject.GetComponent<SpriteRenderer> ();
+		enemySprite.enabled = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void showEnemy() {
+		enemySprite.enabled = true;
+		StartCoroutine (delay());
+	}
+	public void hideEnemy() {
+		enemySprite.enabled = false;
+	}
+	IEnumerator delay() {
+		yield return new WaitForSeconds (showTime);
+		hideEnemy ();
+	}
+}
---
+++ Assets/Finiko/Scripts/Radar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Radar : MonoBehaviour {
+
+	public CircleCollider2D collider;
+	public float speed;
+	public float maxRadius;
+	public string enemyTag;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ResetRadar ();
+	}
+
+	public void UsingRadar(){
+		ResetRadar ();
+		DOTween.To(() => collider.radius, x => collider.radius = x, maxRadius, speed);
+	}
+	public void ResetRadar() {
+		if (collider.radius >= maxRadius) {
+			collider.radius = 0;
+		}
+	}
+	public bool isDetectEnemy() {
+		return (true);
+	}
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag (enemyTag)) {
+			other.GetComponent<Enemy> ().showEnemy ();
+		}
+	}
+}
