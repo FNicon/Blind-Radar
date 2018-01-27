d35bf34c5de0809620edc369b05aaf903136d462
Edrick Ezra <edrickezrazacharia@gmail.com>
2018-01-27 17:46:16 +0700

    Add torpedo selfdestroy

--- Assets/_Scripts/Torpedo.cs
+++ Assets/_Scripts/Torpedo.cs
@@ -7,9 +7,11 @@ public class Torpedo : MonoBehaviour {
 
 	public float incSpeed = 1.0f, maxSpeed = 10.0f;
     public float damage;
+	public float disappearTime = 10.0f;
     public GameObject hitfx;
 	private Rigidbody2D rigidBody;
 	private GameObject particle;
+	private float curTime;
 
 	// Use this for initialization
 	void Start () {
@@ -17,12 +19,20 @@ public class Torpedo : MonoBehaviour {
 		particle = gameObject.transform.GetChild (0).gameObject;
 
 		particle.transform.localEulerAngles = new Vector3 ((transform.eulerAngles.z * -1 - 90), -90, -90);
+		curTime = 0;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		//if (rigidBody.velocity.x < maxSpeed)
 		//	rigidBody.velocity += new Vector2 (incSpeed, 0);
+
+		curTime += Time.deltaTime;
+		if (curTime > disappearTime) {
+			Instantiate(hitfx, transform.position, transform.rotation);
+			Destroy (this.gameObject);
+		}
+
 		if (rigidBody.velocity.magnitude < maxSpeed)
 			rigidBody.AddForce (transform.right * -incSpeed * 50);
 	}
